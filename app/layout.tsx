e2e217f917a2d8fd6d734bacfc506ea09b704e4e
Dương Đào Quang <102663176+Duong-DQ@users.noreply.github.com>
2025-09-12 17:57:00 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,25 +13,26 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Bet2k | Đánh bạc trực tuyến, không lộ danh tính, rút tiền ngay và luôn",
-  description: "Bet2k là nơi hội tụ game bài, bắn cá, nổ hũ siêu hot 2025. Cược dễ, thắng lớn, rút tiền cực nhanh, thưởng đậm mỗi ngày cho mọi tay chơi thực thụ.",
-  openGraph: {
-    title: "Bet2k | Đánh bạc trực tuyến, không lộ danh tính, rút tiền ngay và luôn",
-    description: "Bet2k là nơi hội tụ game bài, bắn cá, nổ hũ siêu hot 2025. Cược dễ, thắng lớn, rút tiền cực nhanh, thưởng đậm mỗi ngày cho mọi tay chơi thực thụ.",
-    url: "/",
-    siteName: "Bet2k",
-    images: [
-      {
-        // url: "/meta-image.webp",
-        url: "https://achulen.ru.com/wp-content/uploads/2025/04/bet88-choi-vui-thuong-lon-dang-ky-nhan-ngay-99k-1.webp",
-        width: 1200,
-        height: 630,
-        type: "image/webp",
-      },
-    ],
-    locale: "vi_VN",
-    type: "website",
-  }
+ title: "Phản ánh của người dân địa bàn Hải Phòng về công tác phát...",
+description: "Theo tài khoản Facebook cá nhân Bao Nguyen The chia sẻ: “CÁN BỘ PHÁT TIỀN 100.000 LỄ 2 THÁNG 9. CÁN BỘ PHƯỜNG LÀM KHÓ NGƯỜI DÂN. KHÔNG THỂ CHẤP NHẬN ĐƯỢC CÁN BỘ...”",
+
+openGraph: {
+  title: "Phản ánh của người dân địa bàn Hải Phòng về công tác phát tiền...",
+  description: "Theo tài khoản Facebook cá nhân Bao Nguyen The chia sẻ: “CÁN BỘ PHÁT TIỀN 100.000 LỄ 2 THÁNG 9. CÁN BỘ PHƯỜNG LÀM KHÓ NGƯỜI DÂN. KHÔNG THỂ CHẤP NHẬN ĐƯỢC CÁN BỘ...”",
+  url: "/",
+  siteName: "Báo Hải Phòng",
+  images: [
+    {
+      url: "https://cdn.discordapp.com/attachments/887124159694524437/1416013458897375333/IMG_3873.jpg?ex=68c54ccc&is=68c3fb4c&hm=5e7c0ee9034a7f595c201d9e9dc33cc882d9bce8a680ac293f3e12b1f77989b8&",
+      width: 1200,
+      height: 630,
+      type: "image/jpeg",
+    },
+  ],
+  locale: "vi_VN",
+  type: "article",
+}
+
 
 };
 
